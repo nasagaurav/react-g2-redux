be0dfbcd230118a31269ae9be8bf02a7bdf151dd
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-18 18:07:27 +0530

    8 components added in app.js

--- src/App.js
+++ src/App.js
@@ -11,8 +11,15 @@ import ViewNumbers from './ViewCounter';
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+<ViewNumbers />
+<ViewTopic />
+<ViewTodos />
+<ViewCounter />
+<AddNumbers />
+<AddTopic />
+<AddTodos /> 
+<AddCounter />
+
     </div>
   );
 }
