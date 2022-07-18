2a709f297e45db29acf03d983e8a3957f1c1b533
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-18 18:00:07 +0530

    add import in app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import ViewNumbers from './ViewNumbers';
+import ViewNumbers from './ViewTopic';
+import ViewNumbers from './ViewTodos';
+import ViewNumbers from './ViewCounter';
+import ViewNumbers from './ViewNumbers';
+import ViewNumbers from './ViewTopic';
+import ViewNumbers from './ViewTodos';
+import ViewNumbers from './ViewCounter';
 
 export default function App() {
   return (
